560679fd54e5502919d6b6723590b08be6c71faf
unknown <daleMyhreProg@gmail.com>
2019-04-13 19:46:47 -0700

    Slight change to test auto win

--- Assets/Scripts/SlotMachine.cs
+++ Assets/Scripts/SlotMachine.cs
@@ -37,6 +37,7 @@ public class SlotMachine : MonoBehaviour {
         if(isFirstSpin)
         {
             isFirstSpin = false;
+            Debug.Log("Cheat Win");
             return possibleSpins.Spins[2];
         }
         else
